ff85dd3c25b0e0380a0191906a088d072d696695
Galu <galluppi.leandro@hotmail.com>
2021-01-17 20:24:05 -0300

    Se agrega vista de arte

--- webpack.config.js
+++ webpack.config.js
@@ -119,7 +119,7 @@ module.exports = (env, argv) => ({
             dry: argv.mode === 'development'
         }),
 
-        ...['index', 'gallery'].map(fileName =>
+        ...['index', 'gallery', 'art'].map(fileName =>
         new HtmlWebpackPlugin({
             minify: argv.mode !== 'development',
             filename: `${fileName}.html`,
