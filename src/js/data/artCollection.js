bccce4a80f6066bbf4749aa6be02459a547528cc
galluppi.leandro@hotmail.com <galluppi.leandro@hotmail.com>
2021-08-16 20:13:25 -0300

    Actualización 8/16/2021 8:13:24 PM

--- src/js/data/artCollection.js
+++ src/js/data/artCollection.js
@@ -1,154 +1,43 @@
-import _art0b3970cbf66e4896805a2be15822410f_ from "../../assets/art/art2.jpg";
-import _artc47b4efa054c43dc8e176b757edd9069_ from "../../assets/art/wallhaven-dgy9po.png";
-import _art9258d50bed374dd7839e72d62b4c64eb_ from "../../assets/art/art3.jpg";
-import _art9c21a36836a94d0ea2ee799fdfb626ac_ from "../../assets/art/art5.jpg";
-import _arte5670489e0174521b8880aa3db01922a_ from "../../assets/art/art8.jpg";
-import _art4e6feac8a4d94a0fb5c74befa7a5bc47_ from "../../assets/art/art11.jpg";
-import _art22a7563fa3ac4c7eb9c5804673465ab6_ from "../../assets/art/art9.jpg";
-import _art44849f58384646169b2a671043969013_ from "../../assets/art/art10.jpg";
-import _art2e5f09a29e464df2a00ea0e87f572fa4_ from "../../assets/art/art13.jpg";
-import _art7a2adbbfa341402894879680880c9f74_ from "../../assets/art/art12.jpg";
-import _artdc5c94c897a2483594a33c2e3e97ac2c_ from "../../assets/art/art1.jpg";
+import _arte7c9e7754124455695e49076e393881a_ from "../../assets/art/b71b1691656c5e2c153e51d46c3d82c5.jpg";
+import _artbd860725d6c749e49e8060ca61522777_ from "../../assets/art/abduction-of-the-alebrije.jpg";
+import _art2d56400bbb7a40c3a70413b4e5a88379_ from "../../assets/art/abduction-of-the-alebrije-LINEART.jpg";
+import _art20d7a6a19cfd4db3822c77c81f42fcdf_ from "../../assets/art/aalebrije.jpg";
+import _art17f041f7eedb4c95830e15e12825838e_ from "../../assets/art/9a7d5b1f134cc42ad96604b3024dfdf3.jpg";
 
 let collection = [
   {
     "id": 1,
     "category": "comic",
-    "title": "Congos",
-    "description": "Imagenes de los congos y un mensaje social",
+    "title": "Would you like to repeat?",
+    "description": "Comic de prueba",
     "technicalDetails": null,
-    "thumbnail": _art0b3970cbf66e4896805a2be15822410f_,
+    "thumbnail": _arte7c9e7754124455695e49076e393881a_,
     "images": [
-      _art0b3970cbf66e4896805a2be15822410f_,
-      _artc47b4efa054c43dc8e176b757edd9069_
+      _arte7c9e7754124455695e49076e393881a_
     ]
   },
   {
     "id": 2,
     "category": "illustration",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
+    "title": "Abduction of the alebrije",
+    "description": "Ilustración de prueba",
     "technicalDetails": null,
-    "thumbnail": _art0b3970cbf66e4896805a2be15822410f_,
+    "thumbnail": _artbd860725d6c749e49e8060ca61522777_,
     "images": [
-      _art0b3970cbf66e4896805a2be15822410f_
+      _artbd860725d6c749e49e8060ca61522777_,
+      _art2d56400bbb7a40c3a70413b4e5a88379_,
+      _art20d7a6a19cfd4db3822c77c81f42fcdf_
     ]
   },
   {
     "id": 3,
     "category": "fanart",
-    "title": "Moiras",
-    "description": "Esta son las tres moiras",
+    "title": "Legendary trio",
+    "description": "Fanart de prueba",
     "technicalDetails": null,
-    "thumbnail": _art9258d50bed374dd7839e72d62b4c64eb_,
+    "thumbnail": _art17f041f7eedb4c95830e15e12825838e_,
     "images": [
-      _art9258d50bed374dd7839e72d62b4c64eb_,
-      _art9c21a36836a94d0ea2ee799fdfb626ac_,
-      _art0b3970cbf66e4896805a2be15822410f_
-    ]
-  },
-  {
-    "id": 4,
-    "category": "fanart",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
-    "technicalDetails": null,
-    "thumbnail": _arte5670489e0174521b8880aa3db01922a_,
-    "images": [
-      _arte5670489e0174521b8880aa3db01922a_,
-      _art4e6feac8a4d94a0fb5c74befa7a5bc47_
-    ]
-  },
-  {
-    "id": 5,
-    "category": "comic",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
-    "technicalDetails": null,
-    "thumbnail": _art22a7563fa3ac4c7eb9c5804673465ab6_,
-    "images": [
-      _art22a7563fa3ac4c7eb9c5804673465ab6_
-    ]
-  },
-  {
-    "id": 6,
-    "category": "illustration",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
-    "technicalDetails": null,
-    "thumbnail": _art44849f58384646169b2a671043969013_,
-    "images": [
-      _art44849f58384646169b2a671043969013_,
-      _art2e5f09a29e464df2a00ea0e87f572fa4_,
-      _art9258d50bed374dd7839e72d62b4c64eb_
-    ]
-  },
-  {
-    "id": 7,
-    "category": "comic",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
-    "technicalDetails": null,
-    "thumbnail": _art4e6feac8a4d94a0fb5c74befa7a5bc47_,
-    "images": [
-      _art4e6feac8a4d94a0fb5c74befa7a5bc47_
-    ]
-  },
-  {
-    "id": 8,
-    "category": "illustration",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
-    "technicalDetails": null,
-    "thumbnail": _art7a2adbbfa341402894879680880c9f74_,
-    "images": [
-      _art7a2adbbfa341402894879680880c9f74_
-    ]
-  },
-  {
-    "id": 9,
-    "category": "comic",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
-    "technicalDetails": null,
-    "thumbnail": _art2e5f09a29e464df2a00ea0e87f572fa4_,
-    "images": [
-      _art2e5f09a29e464df2a00ea0e87f572fa4_,
-      _artdc5c94c897a2483594a33c2e3e97ac2c_
-    ]
-  },
-  {
-    "id": 10,
-    "category": "fanart",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
-    "technicalDetails": null,
-    "thumbnail": _art9258d50bed374dd7839e72d62b4c64eb_,
-    "images": [
-      _art9258d50bed374dd7839e72d62b4c64eb_
-    ]
-  },
-  {
-    "id": 11,
-    "category": "comic",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
-    "technicalDetails": null,
-    "thumbnail": _arte5670489e0174521b8880aa3db01922a_,
-    "images": [
-      _arte5670489e0174521b8880aa3db01922a_,
-      _art44849f58384646169b2a671043969013_
-    ]
-  },
-  {
-    "id": 12,
-    "category": "illustration",
-    "title": "Test Images",
-    "description": "dustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha",
-    "technicalDetails": null,
-    "thumbnail": _art22a7563fa3ac4c7eb9c5804673465ab6_,
-    "images": [
-      _art22a7563fa3ac4c7eb9c5804673465ab6_
+      _art17f041f7eedb4c95830e15e12825838e_
     ]
   }
 ];
